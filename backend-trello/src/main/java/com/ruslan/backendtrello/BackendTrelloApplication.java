18808f684dd861aa750dd4cbb1de66dab2cb0fe7
Ruslan <r.addsky@yahoo.com>
2023-03-26 18:52:51 +0300

    init

---
+++ backend-trello/src/main/java/com/ruslan/backendtrello/BackendTrelloApplication.java
@@ -0,0 +1,13 @@
+package com.ruslan.backendtrello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTrelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTrelloApplication.class, args);
+	}
+
+}
---
+++ backend-trello/src/test/java/com/ruslan/backendtrello/BackendTrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruslan.backendtrello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTrelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
