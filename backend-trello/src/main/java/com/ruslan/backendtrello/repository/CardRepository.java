f2dc0ba5859cced7698fc847c02278ccdee3e261
Ruslan <r.addsky@yahoo.com>
2023-04-02 12:47:16 +0300

    list PUT/DELETE added

--- backend-trello/src/main/java/com/ruslan/backendtrello/repository/CardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ruslan.backendtrello.repository;
-
-import com.ruslan.backendtrello.models.mongo.Card;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CardRepository extends MongoRepository<Card, Long> {
-}
